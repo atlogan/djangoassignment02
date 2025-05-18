2b2553e90676625797f266629a6cf6008dd59a8e
atlogan <drewthomaslogan5201@gmail.com>
2025-05-18 16:34:53 -0400

    initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.StudentListView.as_view(), name='student_list'),
+    path('<int:pk>', views.StudentDetailView.as_view(), name='student_detail'),
+    path('create/', views.StudentCreateView.as_view(), name='student_create')
+]
