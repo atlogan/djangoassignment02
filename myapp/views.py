b14b38cc9fa6977d4def388e833017d232ac883c
atlogan <drewthomaslogan5201@gmail.com>
2025-05-18 17:10:07 -0400

    added update view and create button to list view

--- myapp/views.py
+++ myapp/views.py
@@ -18,14 +18,15 @@ class StudentCreateView(CreateView):
     success_url = reverse_lazy('student_list') # Redirect after successful creation
     # Alternatively define get_success_url(self) method
 
-"""
 
 class StudentUpdateView(UpdateView):
     model = Student
     template_name = 'myapp/student_form.html' # Can reuse the create form template
-    fields = ['field1', 'field2', ...] # Specify fields
-    success_url = reverse_lazy('myapp:student_list') # Redirect after successful update
+    fields = ['name', 'major', 'enrollment_date'] # Specify fields
+    success_url = reverse_lazy('student_list') # Redirect after successful update
 
+
+"""
 class StudentDeleteView(DeleteView):
     model = Student
     template_name = 'myapp/student_confirm_delete.html' # Optional: Defaults to myapp/student_confirm_delete.html
